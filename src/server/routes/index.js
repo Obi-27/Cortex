1f29a0f57e32006267a2dce7f8f39aba92ff7826
obi brown <obrown1000@gmail.com>
2025-08-04 14:06:28 -0400

    added folder endpoints to express router

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import notes from "./notes.js"
+import folders from "./folders.js"
 
 
 const router = express.Router()
 
 router.use('/notes', notes)
+router.use('/folders', folders)
 
 
 export default router
