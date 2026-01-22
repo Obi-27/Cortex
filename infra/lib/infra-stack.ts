73778d6f1ff41b072ed786e0e49d3e96c1c180e7
obi brown <obrown1000@gmail.com>
2026-01-21 22:39:16 -0500

    basic cdk and dployment

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { NotesBucket } from './constructs/notes-bucket';
+import { NotesLambda } from './constructs/notes-lambda';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+
+    const environment = this.node.tryGetContext('env');
+    console.log(environment)
+    if (!environment) {
+      throw new Error('Context variable "env" is required (dev | prod)');
+    }
+
+    const envConfig = this.node.tryGetContext(environment);
+    if (!envConfig) {
+      throw new Error(`No config found for environment: ${environment}`);
+    }
+
+    const notesBucket = new NotesBucket(this, 'NotesBucket', {
+      bucketName: envConfig.bucketName,
+    });
+
+    new NotesLambda(this, 'NotesLambda', {
+      notesBucket: notesBucket.bucket,
+      environment,
+    });
+  }
+}
