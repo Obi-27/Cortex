e83e4fb10dc52d6fe3e5b53f664d3bc880f065cc
obi brown <obrown1000@gmail.com>
2026-01-24 19:02:27 -0500

    added basic backend routing

---
+++ app/backend/index.ts
@@ -0,0 +1,9 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { route } from './router';
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log('Incoming event:', JSON.stringify(event));
+  return route(event);
+};
